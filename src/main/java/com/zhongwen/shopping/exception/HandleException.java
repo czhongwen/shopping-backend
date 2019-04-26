16d7183f7bda9aa3c40ebcb1a47b52e5e47709a3
caozw <caozw@tuya.com>
2019-04-26 10:58:24 +0800

    增加异常处理

--- src/main/java/com/zhongwen/shopping/exception/HandleException.java
+++ src/main/java/com/zhongwen/shopping/exception/HandleException.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 
 /**
@@ -13,15 +15,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
  * @version 1.0
  * @data 2019-04-03 17:41
  **/
+@ResponseBody
 @ControllerAdvice
 public class HandleException {
 
     private final static Logger logger  = LoggerFactory.getLogger(HandleException.class);
 
+    @ExceptionHandler(Exception.class)
+    public ResultVO unknownException(Exception e) {
+        System.out.println("未知异常");
+        return null;
+    }
+
     @ExceptionHandler(RuntimeException.class)
     public ResultVO runtimeExceptionHandler(RuntimeException ex) {
         logger.info("运行异常 {}", ex);
-        System.out.println("异常了");
-        return ResultGenerator.failResult(ex.getMessage(), ex.getCause());
+        System.out.println("运行异常!");
+        return  ResultGenerator.failResult(ex.getMessage(), ex.getCause());
     }
 }
