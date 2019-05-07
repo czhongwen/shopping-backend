41a012aa74590608e6c692cf06f2a01ffd3984fd
caozw <caozw@tuya.com>
2019-05-07 23:21:44 +0800

    完善模版消息

--- src/main/java/com/zhongwen/shopping/service/impl/OrderServiceImpl.java
+++ src/main/java/com/zhongwen/shopping/service/impl/OrderServiceImpl.java
@@ -158,8 +158,12 @@ public class OrderServiceImpl implements IOrderService {
         //查询出商品数量
         List<ProductInfoBean> productInfoBeans = productInfoDAO.getProducts(productIds);
 
+        List<String> productNames = new ArrayList<>();
+        Integer totalCount = 0;
         //判断库存是否满足用户购买
         for (ProductInfoBean productInfo: productInfoBeans) {
+            productNames.add(productInfo.getName());
+            totalCount += map.get(productInfo.getId()) * productInfo.getPrice();
             if (productInfo.getNum() < map.get(productInfo.getId())) {
                 throw new RuntimeException("对不起！您所购买的" + productInfo.getName() + "库存不足，仅剩下" + productInfo.getNum() + "件");
             } if (productInfo.getStatus() == 0) {
@@ -202,7 +206,7 @@ public class OrderServiceImpl implements IOrderService {
         }
         cartInfoDAO.delCartsByIds(ids);
         //发送模版信息
-        this.sendTemplatePaySuccess(openId,formId,orderId, addressId);
+        this.sendTemplatePaySuccess(openId,formId,orderId, addressId, productNames, totalCount);
         return true;
     }
 
@@ -220,7 +224,7 @@ public class OrderServiceImpl implements IOrderService {
         return obj.getString("access_token");
     }
 
-    public void sendTemplatePaySuccess(String openId, String formId,Integer orderId, Integer addressId){
+    public void sendTemplatePaySuccess(String openId, String formId,Integer orderId, Integer addressId, List<String> productNames, Integer totalCount){
         String token = this.getAccessToken();
         String url = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=" + token;
 
@@ -228,7 +232,7 @@ public class OrderServiceImpl implements IOrderService {
         prams.put("touser", openId );
         prams.put("template_id", "vdknVhzNHj5n0esLDs_EzAw3H2xnBkT_X7jcFJmYgBM");
         prams.put("form_id", formId );
-        prams.put("page", "myOrders");
+        prams.put("page", "page/myOrders");
 
         JSONObject obj = new JSONObject();
 
@@ -241,7 +245,7 @@ public class OrderServiceImpl implements IOrderService {
         obj.put("keyword2", obj2);
 
         JSONObject obj3 = new JSONObject();
-        obj3.put("value", "1000元");
+        obj3.put("value", (totalCount - 20) + "元");
         obj.put("keyword3", obj3);
 
         AddressInfoBean addressInfoBean = addressInfoDAO.getById(addressId);
@@ -251,11 +255,12 @@ public class OrderServiceImpl implements IOrderService {
         obj.put("keyword4", obj4);
 
         JSONObject obj5 = new JSONObject();
-        obj4.put("value", addressInfoBean.getProvice() + addressInfoBean.getCity() + addressInfoBean.getCountry() + addressInfoBean.getDetail());
+        obj5.put("value", addressInfoBean.getProvice() + addressInfoBean.getCity() + addressInfoBean.getCountry() + addressInfoBean.getDetail());
         obj.put("keyword5", obj5);
 
         JSONObject obj6 = new JSONObject();
-        obj4.put("value", "");
+        String res =productNames.toString().substring(1,productNames.toString().length()-1);
+        obj6.put("value", res);
         obj.put("keyword6", obj6);
 
         prams.put("data", obj);
